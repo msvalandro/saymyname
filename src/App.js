dced7a48630a8eaec41a28d9835c345b361c707a
Matheus Valandro <msvalandro@gmail.com>
2020-03-01 17:17:24 -0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
+      <Header></Header>
       <Home></Home>
       <GlobalStyle></GlobalStyle>
     </>
---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  margin: 50px 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
