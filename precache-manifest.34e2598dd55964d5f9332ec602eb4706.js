d2bfda66e1e7a0881db6f68d1c2be3c076756d20
matheus_dorneles <matheus_dorneles@terceiros.sicredi.com.br>
2020-03-02 08:57:59 -0300

    Updates

---
+++ precache-manifest.34e2598dd55964d5f9332ec602eb4706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256d17a7345c1f51625344e43e76f9ca",
+    "url": "/saymyname/index.html"
+  },
+  {
+    "revision": "ee45dc99a3b57bcf1482",
+    "url": "/saymyname/static/js/2.8aefe265.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/saymyname/static/js/2.8aefe265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f80e2e43305cbbdd512",
+    "url": "/saymyname/static/js/main.982989d8.chunk.js"
+  },
+  {
+    "revision": "ba0c0b1bdc2ed4c1294b",
+    "url": "/saymyname/static/js/runtime-main.9535c799.js"
+  },
+  {
+    "revision": "018e896037f7c5c627de473e127db7b6",
+    "url": "/saymyname/static/media/background.018e8960.svg"
+  },
+  {
+    "revision": "e9c184aaed1c0ad3b3549a817050ef26",
+    "url": "/saymyname/static/media/logo.e9c184aa.svg"
+  }
+]);
